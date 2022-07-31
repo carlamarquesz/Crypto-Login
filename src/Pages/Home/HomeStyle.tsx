b5d04e3566d9379fa4e2d7db0b44b8f514d426e1
Carla Marques <marques.carl97@gmail.com>
2022-07-31 18:15:32 -0300

    pagina-login

--- src/Pages/Home/HomeStyle.tsx
+++ src/Pages/Home/HomeStyle.tsx
@@ -1,65 +1,61 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  flex: 1;
+  display: flex; 
+  min-height: 100vh; 
+  background: #211D28;
+  justify-content: flex-start;
   flex-direction: column;
   align-items: center;
-  min-height: 100vh;
-  min-width: 100vw;
-  background-color: ${({ theme }) => theme.colors.secondary};
 `;
 
-export const MainContent = styled.main`
-  display: flex;
-  align-self: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100%;
-  background: linear-gradient(purple, transparent);
+export const Title = styled.h1`
+  color: white;
+`;
+export const Subtitle = styled.h2`
+  opacity: 0.5;
+  color: white; 
+`;
+export const Input = styled.input`
+  border: 1px #575353 solid;
+  background: transparent;
+  border-radius: 8px 8px 0 0;
+  min-width: 20rem;
+  height: 2rem;
+  color: #575353; 
+  padding: 10px;
+  :hover{
+    border-color: #bb1fd2;
+  } 
 `;
 
-export const DonateForm = styled.form`
+export const InputPassword = styled(Input)` 
+  border-radius: 0 0 8px 8px; 
+`;
+
+export const ContainerForgot = styled.button` 
+  min-width: 20rem; 
+  height: 3rem;
   display: flex;
-  flex-direction: column;
-
-  section:first-of-type {
-    justify-content: center;
-  }
-
-  section {
-    display: flex;
-    align-items: flex-end;
-    flex: 1;
-    width: 100%;
-    margin-top: 1rem;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin: -0.5rem;
-    margin-top: 0.5rem;
-    & > * {
-      margin: 0.5rem;
-      @media screen and (max-width: 360px) {
-        margin-top: 1rem;
-      }
-    }
-
-    & > button {
-      min-width: 4rem;
-      align-items: center;
-      align-self: stretch;
-      @media screen and (max-width: 360px) {
-        flex: 1;
-      }
-      &[type='submit'] {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 1.5rem;
-        & > svg {
-          fill: #fff;
-        }
-      }
-    }
-  }
+  justify-content: flex-end;
+  color: #bb1fd2;
+  font-size:1rem; 
+  background: none;
+  border: none; 
+`;
+export const TextForgot = styled.h2`  
+  color: #bb1fd2;
+  font-size:1rem;  
+`;
+export const ButtonLogin = styled.button`
+  min-width: 20rem;
+  min-height: 3rem;
+  background: linear-gradient(135deg, #9a4dff 0%, #f600dd 100%);
+  box-shadow: 0px 4px 20px rgba(188, 49, 243, 0.5);
+  border-radius: 8px;
+  border: none;
+  color: white;
+  font-size: 1rem;
+  line-height: 20px;
+  font-weight: 700;
 `;
